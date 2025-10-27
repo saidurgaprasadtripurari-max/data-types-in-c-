895691ec298bf3db26049a3fefa5cff7f04fc7bb
saidurgaprasadtripurari-max <saidurgaprasadtripurari@gmail.com>
2025-10-27 13:39:09 +0530

    Add files via upload

---
+++ RELATIONAL OPERATOR.cpp
@@ -0,0 +1,18 @@
+// RELATIONAL OPERATOR
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+    int a = 20, b = 10;
+
+    printf("%d\n", a <  b); // LESS THAN
+    printf("%d\n", a >  b); // GREATER THAN
+    printf("%d\n", a <= b); // LESS THAN OR EQUAL
+    printf("%d\n", a >= b); // GREATER THAN OR EQUAL
+    printf("%d\n", a != b); // NOT TRUE
+    printf("%d\n", a == b); // COMPARISION 
+    
+
+    return 0;
+}
+
